fcf1159728438a5e9f554851f631b2367998c28a
EC2 Default User <ec2-user@ip-172-31-67-193.ec2.internal>
2023-01-26 21:08:49 +0000

    Lecture2

---
+++ Lecture2.py
@@ -0,0 +1,26 @@
+#print(type('hello')) #single line comment
+#print(type(123))
+#print(type(123.123))
+
+#print('hello')
+#print("hello")
+#print("it's our 2nd python class")
+
+#print('Hello World')
+
+#print('Hello World'.lower())
+
+#print('hello world'.strip())
+
+#print('hello word')
+#print('hello word'.split())     #default, split by spaces
+#print('hello word'.split('o'))  #split by "o"
+
+#print('hello '+'world')
+#print(1+2) #Simple addition but also subraction, multiplication, division, etc.
+
+my_num=123
+print(my_num)
+
+my_num=234
+print(my_num)
